45aefdd4a6e10011087a33b349ef60b4f136f4dd
Bryan <bryan.knouse@gmail.com>
2014-10-01 21:30:21 -0400

    first intraworlds commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Intraworlds::Application.config.secret_key_base = '57dd165d2b7fd4f6bd80914c2d90ac69c3782f5342d52d30b3bc8d3e5e0492e9d9c690d08fd8c0ba9fac4e0d40db3948e0e634706431c15175a01626d9bffdbc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Intraworlds::Application.config.session_store :cookie_store, key: '_intraworlds_session'
